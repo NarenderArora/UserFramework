4641c693bbed3c6b37d58aa2b08b642ef00cf402
NarenderArora <58692195+NarenderArora@users.noreply.github.com>
2025-04-23 19:05:57 +0530

    framework added1

---
+++ UserFramwork/UserFramwork/AppModule/UserData.swift
@@ -0,0 +1,21 @@
+//
+//  UserData.swift
+//  UserFramwork
+//
+//  Created by A1TM1WZ7 on 22/04/25.
+//
+
+import Foundation
+
+public struct UserData {
+    public init() {
+        
+    }
+    public func getUserName() -> String {
+        return "UserFramework"
+    }
+    
+    public func getUserAge() -> Int {
+        return 30
+    }
+}
---
+++ UserFramwork/UserFramwork/UserFramwork.h
@@ -0,0 +1,18 @@
+//
+//  UserFramwork.h
+//  UserFramwork
+//
+//  Created by A1TM1WZ7 on 22/04/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UserFramwork.
+FOUNDATION_EXPORT double UserFramworkVersionNumber;
+
+//! Project version string for UserFramwork.
+FOUNDATION_EXPORT const unsigned char UserFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserFramwork/PublicHeader.h>
+
+
